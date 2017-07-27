e7413d8ce8f36e9b0b01c4edaaad8f438f87d6ba
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-07-27 22:21:02 +0200

    Problem 3 solved.

--- 3/3.py
+++ 3/3.py
@@ -1,3 +1,18 @@
-def isPrime(number):
-    for i in range(2, math.sqrt(number)):
-        return True;
+import math
+
+
+def is_prime(number):
+    if number == 1:
+        return False
+    for denominator in range(2, int(math.sqrt(number))):
+        if number % denominator == 0:
+            return False
+    return True
+
+number = 600851475143
+
+for i in range(1, int(math.sqrt(number))):
+    if is_prime(i) and number % i == 0:
+        x = i
+
+print(x)
