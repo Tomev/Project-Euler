8e065ff7d7d4717636d8fa5b7cefe907dd6dd4df
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-07-28 12:38:19 +0200

    [STABLE] Added solution to problem 4.

---
+++ 4/4.py
@@ -0,0 +1,17 @@
+def is_palindrome(string):
+    i = 0
+    while len(string) - 1 - i > i:
+        if string[len(string)- i - 1] != string[i]:
+            return False
+        i += 1
+    return True
+
+x = 0
+
+for i in range(999, 100, -1):
+    for j in range(999, 100, -1):
+        if is_palindrome(str(i*j)):
+            if i * j > x:
+                x = i * j
+
+print(x)
