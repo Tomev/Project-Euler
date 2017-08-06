55a16247124d84272d8a9ac579730789b8daf895
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-08-06 14:37:50 +0200

    [STABLE] Added solution to problem 12.

---
+++ 12/12.py
@@ -0,0 +1,29 @@
+import math
+
+
+def count_number_of_divisors(number):
+
+    divisors_num = 0
+    div = 1
+
+    while div <= int(math.sqrt(number)):
+        if number % div == 0:
+            if div == math.sqrt(number):
+                divisors_num += 1
+            else:
+                divisors_num += 2
+        div += 1
+
+    return divisors_num
+
+triangle_number = 1
+i = 2
+
+while True:
+    div_num = count_number_of_divisors(triangle_number)
+    if div_num > 500:
+        break
+    triangle_number += i
+    i += 1
+
+print(triangle_number)
