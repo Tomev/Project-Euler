444e8144e08466f48edd02c650ed38049185cd72
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-08-10 01:58:02 +0200

    [STABLE] Added solution to problem 14.

---
+++ 14/14.py
@@ -0,0 +1,21 @@
+def get_Collatz_sequence_length(number):
+    if number == 1:
+        return 1
+    if number % 2 == 0:
+        return 1 + get_Collatz_sequence_length(number / 2)
+    else:
+        return 1 + get_Collatz_sequence_length(3* number + 1)
+
+
+longest_length = 0
+longest_length_number = 0
+
+for i in range(1, 1000000):
+
+    length_of_i = get_Collatz_sequence_length(i)
+
+    if length_of_i > longest_length:
+        longest_length = length_of_i
+        longest_length_number = i
+
+print(longest_length_number)
