c4d3476817d65f74a1040719a1c7ec67174ac6ce
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-04-05 11:34:59 +0200

    [STABLE] Added full solution to problem 23.

--- 23/23.py
+++ 23/23.py
@@ -27,22 +27,26 @@ def is_double_abundant_sum(number, abudantNums):
                 return True
     return False
 
-
-abundantNumbers = []
+abundant_numbers = []
 
 last_non_double_abundant_sum_number = 28123
+all_numbers_considered_sum = 0
+
+print("Finding abundant numbers...")
 
 for i in range(1, last_non_double_abundant_sum_number + 1):
     if is_abundant_number(i):
-        abundantNumbers.append(i)
+        abundant_numbers.append(i)
+    all_numbers_considered_sum += i
 
-print(abundantNumbers)
+print("Finding sums of two abundant numbers in considered range...")
 
-sum_of_non_double_abundant_sum_numbers = 0
+sums_of_two_abundant_numbers = {24}
 
-for i in range(1, last_non_double_abundant_sum_number + 1):
-    print(i)
-    if not is_double_abundant_sum(i, abundantNumbers):
-        sum_of_non_double_abundant_sum_numbers += i
+for i in range(0, len(abundant_numbers)):
+    for j in range(i, len(abundant_numbers)):
+        current_abundant_number_sum = abundant_numbers[i] + abundant_numbers[j]
+        if current_abundant_number_sum <= last_non_double_abundant_sum_number:
+            sums_of_two_abundant_numbers.add(current_abundant_number_sum)
 
-print(sum_of_non_double_abundant_sum_numbers)
+print(all_numbers_considered_sum - sum(sums_of_two_abundant_numbers))
