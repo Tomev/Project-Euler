88e73600157049d37d1c762c2365f3941141bbcd
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-05-09 14:03:59 +0200

    [STABLE] Added solution to problem 29.

---
+++ 29/29.py
@@ -0,0 +1,22 @@
+
+
+def get_function_value(a, b):
+    result = 1
+    for i in range(0, b):
+        result *= a
+    return result
+
+
+MIN_A = 2
+MAX_A = 100
+MIN_B = 2
+MAX_B = 100
+
+values_set = set()
+
+for a in range(MIN_A, MAX_A + 1):
+    print(a)
+    for b in range(MIN_B, MAX_B + 1):
+        values_set.add(get_function_value(a,b))
+
+print(len(values_set))
