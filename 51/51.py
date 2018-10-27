4e2f8348bfce403abd47657ac989f56d09bf25e3
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-10-28 00:54:49 +0200

    [STABLE] Added solution to problem 51.

--- 51/51.py
+++ 51/51.py
@@ -1,8 +1,14 @@
 from methods import get_next_prime, is_prime, get_n_len_combinations
+import time
+
+replaceable_positions_combinations = []
+possible_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+occurred_primes = set()
 
 
 def find_replaceable_positions_combinations(str_length):
-    if str_length < 2: return 0
+    if str_length < 2:
+        return {}
 
     considered_symbols = []
 
@@ -15,71 +21,67 @@ def find_replaceable_positions_combinations(str_length):
     valid_combinations = []
 
     for i in range(0, len(possible_combinations)):
-        for object in possible_combinations[i]:
-            if(len(set(object)) == i + 1):
-                valid_combinations.append(set(object))
+        for obj in possible_combinations[i]:
+            if len(set(obj)) == (i + 1):
+                valid_combinations.append(set(obj))
 
     return valid_combinations
 
 
-def find_combinations_of_other_numbers(combinations_len):
+def get_number_of_prime_family_members(prime):
 
-    symbols = []
+    max_family_size = 0
 
-    for i in range(0, 10):
-        symbols.append(str(i))
+    for replaceable_char_set in replaceable_positions_combinations:
+        current_family_size = get_prime_family_size_after_replacement(prime, replaceable_char_set)
 
-    possible_combinations = get_n_len_combinations(combinations_len, symbols)
+        if current_family_size > max_family_size:
+            max_family_size = current_family_size
 
-    return possible_combinations
-
-
-def find_frontally_valid_combinations_of_other_numbers(combinations_len):
-    possible_combinations = find_combinations_of_other_numbers(combinations_len)
-
-    valid_combinations = []
-
-    for combination in possible_combinations:
-        if not combination[0] == '0':
-            valid_combinations.append(combination)
-
-    return valid_combinations
+    return max_family_size
 
 
-def find_back_valid_combinations_of_other_numbers(combinations_len):
-    possible_combinations = find_combinations_of_other_numbers(combinations_len)
+def get_prime_family_size_after_replacement(prime, replaceable_char_set):
+    family = []
 
-    valid_combinations = []
+    for char in possible_chars:
 
-    for combination in possible_combinations:
-        if not(int(combination) % 2 == 0 or combination[combinations_len -1] == '5'):
-            valid_combinations.append(combination)
+        family_member = str(prime)
 
-    return valid_combinations
+        for replaceable_char_position in replaceable_char_set:
+            family_member = family_member[:int(replaceable_char_position)] + char + family_member[int(replaceable_char_position) + 1:]
+
+        if family_member[0] == '0':
+            continue
+
+        if is_prime(int(family_member)) and len(str(prime)) == len(family_member):
+            family.append(family_member)
+
+    # Ugly check for prime inclusion in family
+    if str(prime) in family:
+        return len(family)
+
+    return 0
 
 
-def find_both_side_valid_combinations_of_other_numbers(combinations_len):
-    possible_combinations = find_back_valid_combinations_of_other_numbers(combinations_len)
+start_time = time.time()
 
-    valid_combinations = []
+max_family_size_occurred = 0
+current_prime_family_size = 0
+current_prime = 2
 
-    for combination in possible_combinations:
-        if not combination[0] == '0':
-            valid_combinations.append(combination)
+while max_family_size_occurred < 8:
 
-    return valid_combinations
-
-
-print(find_both_side_valid_combinations_of_other_numbers(4))
-
-current_prime = get_next_prime(10)
-current_prime_len = len(str(current_prime))
-
-while current_prime_len < 3:
     current_prime = get_next_prime(current_prime + 1)
+    replaceable_positions_combinations = find_replaceable_positions_combinations(len(str(current_prime)))
 
-    if len(str(current_prime)) > current_prime_len:
-        current_prime_len = len(str(current_prime))
-        find_replaceable_positions_combinations(current_prime_len)
+    current_prime_family_size = get_number_of_prime_family_members(current_prime)
 
-print(find_replaceable_positions_combinations(len(str(current_prime))))
+    if current_prime_family_size > max_family_size_occurred:
+        max_family_size_occurred = current_prime_family_size
+
+elapsed_time = time.time() - start_time
+
+print(current_prime)
+print('Time: ' + str(elapsed_time))
+
