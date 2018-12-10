2f6ecaef675a439e43beaa9d5c9aabd3080ca824
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-10 13:49:04 +0100

    Added solution to problem 58.

---
+++ 58/58.py
@@ -0,0 +1,32 @@
+from methods import is_prime
+
+current_last = 1
+
+side_len = 1
+
+travel_by = 1
+
+edges_num = 0
+prime_edges_num = 0
+ratio = 1
+
+while ratio > 0.1:
+    side_len += 2
+    edges_num += 4
+    current_last += 1
+    current_last += travel_by
+    travel_by += 1
+
+    if is_prime(current_last):
+        prime_edges_num += 1
+
+    for i in range(0, 3):
+        current_last += travel_by
+
+        if is_prime(current_last):
+            prime_edges_num += 1
+
+    travel_by += 1
+    ratio = prime_edges_num / edges_num
+
+print(side_len)
