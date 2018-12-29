6591203cf6aaf27765749791585eac8d4f34c97d
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-29 14:37:48 +0100

    Work in progress on problem 60 with dictionaries.

--- Page2/60/60.py
+++ Page2/60/60.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../..')
-from methods import get_next_prime, is_prime, get_n_len_distinct_lists
+from methods import get_next_prime, is_prime
 from time import time
 from itertools import combinations
 
@@ -17,40 +17,146 @@ def create_pairs_of_concatenated_strings(primes):
     return True
 
 
+def add_to_dictionary(main_dict, obj_list):
+    if len(obj_list) == 0:
+        return
+
+    obj_list.sort()
+    key = obj_list[0]
+
+    if not key in main_dict:
+        main_dict[key] = dict()
+
+    add_to_dictionary(main_dict[key], obj_list[1:])
+
+
+def get_list_from_key(main_dict, key):
+
+    if len(main_dict[key].keys()) == 0:
+        return [[key]]
+
+    lists = []
+
+    for k in main_dict[key].keys():
+        for l in get_list_from_key(main_dict[key], k):
+            l.append(key)
+            lists.append(l)
+
+    return lists
+
+
 elapsed_time = time()
 
 primes = []
 current_prime = 2
 number_of_objects_in_combination = 5
+solution_primes = []
+prime_twos = []
+prime_threes = []
+prime_fours = []
 
-while current_prime < 700:
+primes_dict = dict()
+
+while current_prime < 27000:
+
     current_prime = get_next_prime(current_prime + 1)
     primes.append(current_prime)
 
-cmb = combinations(primes, number_of_objects_in_combination)
+    print(current_prime)
 
-for combination in cmb:
+    for key in primes_dict:
+        l = get_list_from_key(primes_dict, key)
+        for primes_list in l:
+            primes_list.append(current_prime)
+            if create_pairs_of_concatenated_strings(primes_list):
+                add_to_dictionary(primes_dict, primes_list)
+
+    for key in primes_dict:
+        if len(get_list_from_key(primes_dict, key)[0]) == 5:
+            print(get_list_from_key(primes_dict, key))
+
+    add_to_dictionary(primes_dict, [current_prime])
+
+
+for key in primes_dict:
+    if len(get_list_from_key(primes_dict, key)) == 5:
+        print(get_list_from_key(primes_dict, key))
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+print(primes_dict[3].keys())
+
+
+for key in primes_dict:
+    print(get_list_from_key(primes_dict, key))
+
+
+cmbs = list(set(combinations(primes, 2)))
+
+for combination in cmbs:
     if create_pairs_of_concatenated_strings(list(combination)):
-        print(str(combination) + ": " + str(sum(list(combination))))
+        l = list(combination)
+        l.sort()
+        prime_twos.append(l)
+
+for primes_two in prime_twos:
+    for prime in primes:
+        if primes_two.__contains__(prime):
+            continue
+        primes_two.append(prime)
+        if create_pairs_of_concatenated_strings(primes_two):
+            prime_threes.append(primes_two[:])
+        primes_two.remove(prime)
+
+for primes_three in prime_threes:
+    for prime in primes:
+        if primes_three.__contains__(prime):
+            continue
+        primes_three.append(prime)
+        if create_pairs_of_concatenated_strings(primes_three):
+            primes_three.sort()
+            prime_fours.append(primes_three[:])
+        primes_three.remove(prime)
+
+#print(set(prime_fours))
+
+cmb = combinations(primes, 4)
+cmb = set(cmb)
+
+print(len(list(cmb)))
 
 
 
 
-#print('Getting combinations')
-#combinations = get_n_len_distinct_lists(5, primes)
-#print('Got')
-'''
-print('Looking through combinations')
-for combination in combinations:
-    if create_pairs_of_concatenated_strings(combination):
+
+
+while len(solution_primes) == 0:
+
+    current_prime = get_next_prime(current_prime + 1)
+    primes.append(current_prime)
+
+    if current_prime < 670:
+        continue
+
+
+
+    for combination in cmbs:
         print(combination)
-'''
 
-#sum_of_primes = 0
 
-#for prime in solution_primes:
-#   sum_of_primes += prime
 
-#print(sum_of_primes)
+
+print(sum(solution_primes))
 elapsed_time = time() - elapsed_time
 print('Time: ' + str(elapsed_time))
+'''
